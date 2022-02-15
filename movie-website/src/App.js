8796bd518883e3153921cc9268c74b262f6af8c9
Si Hyeong Lee <super2451894@gmail.com>
2022-02-15 13:55:09 +0900

    useEffect Function
    - 1회성 코드 작성을 위한 useEffect Function

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -1,11 +1,21 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  console.log("I run all the time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("I run only once.");
+  // };
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back to ReactJS</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
