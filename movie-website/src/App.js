02883877e38e0d35422e3c34241aed34d941c77f
Si Hyeong Lee <super2451894@gmail.com>
2022-02-15 15:12:02 +0900

    useEffect() Function
    - useEffect() 함수

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -2,18 +2,33 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => {
     setValue((prev) => prev + 1);
   };
-  console.log("I run all the time");
-  // const iRunOnlyOnce = () => {
-  //   console.log("I run only once.");
-  // };
+  const onChange = (event) => setKeyword(event.target.value);
+  // console.log("I run all the time");
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here.."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
